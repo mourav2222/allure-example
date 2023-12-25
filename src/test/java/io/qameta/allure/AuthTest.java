24fd4104ef2df6075e74bcd7b4793fa34704c8ee
mourav2222 <mourav2222@gmail.com>
2023-12-25 12:38:04 +0100

    Add new test case on auth (github)

---
+++ src/test/java/io/qameta/allure/AuthTest.java
@@ -0,0 +1,38 @@
+package io.qameta.allure;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
+
+@Feature("Auth")
+@Story("External Auth")
+@Owner("admin")
+class AuthTest {
+
+    @Test
+    @AllureId("2708")
+    @DisplayName("Authentication via GitHub")
+    @Tags({@Tag("critical"), @Tag("web")})
+    void testGitHub() {
+        step("Open main website");
+        step("Select the authentication via GitHub", () -> {
+            step("Press a button Login", () -> {
+
+            });
+            step("Press a button GitHub");
+        });
+        step("Authenticate as user 'testuser'", () -> {
+            step("Input the mail 'testuser@test.com'");
+            step("Input the password '12341234'");
+            step("Press the button Login in");
+        });
+        step("Check the authentication as user 'testuser'", () -> {
+            step("Go back to main page");
+            step("Check user login must be 'testuser'");
+
+        });
+    }
+}
