6d41bf789eef6171c6a1abf2bfbbe0f8d111f47c
mourav2222 <mourav2222@gmail.com>
2023-12-25 18:41:23 +0100

    Add parametrized test

--- src/test/java/io/qameta/allure/AuthTest.java
+++ src/test/java/io/qameta/allure/AuthTest.java
@@ -1,13 +1,50 @@
 package io.qameta.allure;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static io.qameta.allure.Allure.step;
+import static io.qameta.allure.Allure.*;
 
 @Feature("Auth")
 @Story("External Auth")
 @Owner("admin")
 class AuthTest {
+
+    @ParameterizedTest(name = "Authantication via external system")
+    @Tags({@Tag("critical"), @Tag("web")})
+    @CsvSource({
+            "2716, GitHub",
+            "2717, Azure",
+            "2707, Google"
+    })
+    void testAllAuths(String allureID, String typeAuth) {
+        label("AS_ID", allureID);
+        parameter("Type", typeAuth);
+
+        step("Open main website");
+        step("Select the authentication via " + typeAuth, () -> {
+            step("Press a button Login", () -> {
+
+            });
+            step("Press a button " + typeAuth);
+        });
+        step("Authenticate as user 'testuser'", () -> {
+            step("Input the mail 'testuser@test.com'");
+            step("Input the password '12341234'");
+            step("Press the button Login in");
+        });
+        step("Check the authentication as user 'testuser'", () -> {
+            step("Go back to main page");
+            step("Check user login must be 'testuser'");
+            step("The user name must be 'Mr Test'");
+
+        });
+
+        Assertions.assertTrue(true);
+    }
 
     @Test
     @AllureId("2716")
@@ -84,5 +121,6 @@ class AuthTest {
 
         Assertions.assertTrue(true);
     }
+
 
 }
