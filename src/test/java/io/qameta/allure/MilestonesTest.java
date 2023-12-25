ed18fb244afd323a808efe45e4c687e97046d693
mourav2222 <mourav2222@gmail.com>
2023-12-25 20:39:21 +0100

    Add new tests for features Labels and Milestones

--- src/test/java/io/qameta/allure/MilestonesTest.java
+++ src/test/java/io/qameta/allure/MilestonesTest.java
@@ -2,60 +2,24 @@ package io.qameta.allure;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
-import static io.qameta.allure.Allure.*;
+import static io.qameta.allure.Allure.step;
 
-@Layer("integ")
-@Feature("Auth")
-@Story("External Auth")
+@Layer("unit")
+@Feature("Milestones")
+@Story("Stages")
 @Owner("admin")
-class AuthTest {
-
-    @ParameterizedTest()
-    @Tags({@Tag("critical"), @Tag("web")})
-    @CsvSource({
-            "2740, GitHub",
-            "2741, Azure",
-            "2742, Google"
-    })
-    @DisplayName("Authantication via external system")
-    void testAllAuths(String allureID, String typeAuth) {
-        label("AS_ID", allureID);
-        parameter("Type", typeAuth);
-
-        step("Open main website");
-        step("Select the authentication via " + typeAuth, () -> {
-            step("Press a button Login", () -> {
-
-            });
-            step("Press a button " + typeAuth);
-        });
-        step("Authenticate as user 'testuser'", () -> {
-            step("Input the mail 'testuser@test.com'");
-            step("Input the password '12341234'");
-            step("Press the button Login in");
-        });
-        step("Check the authentication as user 'testuser'", () -> {
-            step("Go back to main page");
-            step("Check user login must be 'testuser'");
-            step("The user name must be 'Mr Test'");
-
-        });
-
-        Assertions.assertTrue(true);
-    }
+class MilestonesTest {
 
     @Test
-    @AllureId("2716")
-    @DisplayName("Authentication via GitHub")
-    @Tags({@Tag("critical"), @Tag("web")})
-    void testGitHub() {
-
+    @DisplayName("Milestones via GitHub")
+    @Tags({@Tag("critical"), @Tag("milestones")})
+    void testMilestonesGitHub() {
 
         boolean assertCheck = "False".equalsIgnoreCase(RandomStringUtils.randomAlphabetic(5));
-        step("Open main website (CHANGED)");
+        assertCheck = true;
+
+        step("Milestones: Open main website (CHANGED)");
         step("Select the authentication via GitHub", () -> {
             step("Press a button Login", () -> {
 
@@ -78,11 +42,10 @@ class AuthTest {
     }
 
     @Test
-    @AllureId("2707")
-    @DisplayName("Authentication via Google")
-    @Tags({@Tag("smoke"), @Tag("web")})
-    void testGoogleAuth() {
-        step("Open main website");
+    @DisplayName("Milestones via Google")
+    @Tags({@Tag("smoke"), @Tag("milestones")})
+    void testMilestonesGoogle() {
+        step("Milestones: Open main website");
         step("Select the authentication via Google", () -> {
             step("Press a button Login");
             step("Press a button Google");
@@ -103,11 +66,10 @@ class AuthTest {
 
 
     @Test
-    @AllureId("2717")
-    @DisplayName("Authentication via Azure")
-    @Tags({@Tag("smoke"), @Tag("web")})
-    void testAzureAuth() {
-        step("Open main website");
+    @DisplayName("Milestones via Azure")
+    @Tags({@Tag("smoke"), @Tag("milestones")})
+    void testMilestonesAzure() {
+        step("Milestones: Open main website");
         step("Select the authentication via Azure", () -> {
             step("Press a button Login");
             step("Press a button Azure");
