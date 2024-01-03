bd319c89103d45fb00d5e2a6074f442f5f152b3c
muraviev <m.muraviev@cenit.com>
2024-01-03 15:31:52 +0100

    Manual to automated test workflow

--- src/test/java/io/qameta/allure/AgentsOverviewTest.java
+++ src/test/java/io/qameta/allure/AgentsOverviewTest.java
@@ -8,6 +8,11 @@ import org.junit.jupiter.api.Test;
 class AgentsOverviewTest {
 
     @Test
+    protected void testAAA() {
+
+    }
+
+    @Test
     void agentsFilterByAgentTest() {
 
     }
@@ -36,4 +41,138 @@ class AgentsOverviewTest {
             });
         });
     }
+
+    @AllureId("2842")
+    @Owner("admin")
+    // "msrv": [mounth]
+    @Microservice("mounth")
+    @Test
+    @DisplayName("<2> Manual Test - Changed")
+    @Tags({@Tag("januar"), @Tag("critical"), @Tag("import")})
+    void test2ManualTest() {
+
+        Allure.step("Step 1", () -> {
+            Allure.step("Step 11", () -> {
+            });
+        });
+
+        Allure.step("Step 2", () -> {
+            Allure.step("Step 22", () -> {
+            });
+        });
+
+        Allure.step("Step 3", () -> {
+
+            Allure.step("Step 31", () -> {
+            });
+
+            Allure.step("Step 32", () -> {
+            });
+
+            Allure.step("Step 33", () -> {
+            });
+        });
+
+        Allure.step("Step 4", () -> {
+        });
+
+        Allure.step("Step 5", () -> {
+        });
+    }
+
+    @AllureId("2872")
+    @Owner("admin")
+    @Test
+    @DisplayName("Manual test 004")
+    @Tags({@Tag("critical")})
+    void testManualTest004() {
+
+        Allure.step("Step 1", () -> {
+        });
+
+        Allure.step("Step 2 ", () -> {
+        });
+
+        Allure.step("Step 3", () -> {
+        });
+
+        Allure.step("Step 4 ", () -> {
+        });
+
+        Allure.step("Step 5 ", () -> {
+        });
+    }
+
+    @AllureId("2616")
+    @Owner("admin")
+    @Test
+    @DisplayName("New Test")
+    void testNewTest22() {
+
+        Allure.step("Step 1", () -> {
+        });
+
+        Allure.step("Step2", () -> {
+        });
+
+        Allure.step("Step 3", () -> {
+        });
+
+        Allure.step("Step 4", () -> {
+        });
+        Allure.step("Step 5", () -> {
+        });
+        Allure.step("Step 6", () -> {
+        });
+    }
+
+    @AllureId("2873")
+    @Owner("admin")
+    @Test
+    @DisplayName("New Test")
+    @Tags({@Tag("januar")})
+    void testNewTest() {
+
+        Allure.step("Step 1", () -> {
+        });
+
+        Allure.step("Step 2", () -> {
+        });
+
+        Allure.step("Step3", () -> {
+        });
+        Allure.step("Step 4", () -> {
+        });
+        Allure.step("Step 5", () -> {
+        });
+    }
+
+    @AllureId("2905")
+    @Owner("admin")
+    @Test
+    @DisplayName("New Test 005")
+    void testNewTest005() {
+
+        Allure.step("Step 1", () -> {
+        });
+
+        Allure.step("Step 2", () -> {
+        });
+
+        Allure.step("Step 3", () -> {
+        });
+
+        Allure.step("Step 4", () -> {
+        });
+
+        Allure.step("Step 5", () -> {
+        });
+
+        Allure.step("Step 6", () -> {
+        });
+    }
+
+
+
+
 }
